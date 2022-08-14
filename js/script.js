610daee5e7073bb86f9c762c372ca84e8e5df544
Jeff <soares.jeffersonthadeu@gmail.com>
2022-08-14 10:58:53 -0300

    small html changes & js perguntas frequentes done

--- js/script.js
+++ js/script.js
@@ -1,34 +1,48 @@
-const links = document.querySelectorAll(".header-menu a")
+/* Ativar Links do Menu */
 
+const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(link){
-    const url = window.location.href
-    const href = link.href
+function ativarLink(link) {
+  const url = window.location.href;
+  const href = link.href;
 
-    if(url.includes(href))
-    {
-      link.classList.add("ativo")
-    }
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
 }
 
-links.forEach(ativarLink)
+links.forEach(ativarLink);
 
+// Ativar Itens Orçamento
 
+const parametros = new URLSearchParams(location.search);
 
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
 
+parametros.forEach(ativarProduto);
 
+//Perguntas Frequentes
 
+const perguntas = document.querySelectorAll(".perguntas button");
 
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const constrols = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(constrols);
 
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
 
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
 
-
-
-
-
-
-
-
-
-
-
+perguntas.forEach(eventosPerguntas);
+console.log(perguntas);
